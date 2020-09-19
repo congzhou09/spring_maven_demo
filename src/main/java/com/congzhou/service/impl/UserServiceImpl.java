65eefbed8e7853c4a2e86bab096d003ef93a09d6
congzhou09 <yuancongzhou@yeah.net>
2020-09-19 19:48:13 +0800

    1.Logger改为使用注解方式注入； 2.增加项目配置文件并在Logger中通过@Value读取配置项的值； 3.通过@PostConstruct解决构造函数执行时@Value值还未完成注入；

--- src/main/java/com/congzhou/service/impl/UserServiceImpl.java
+++ src/main/java/com/congzhou/service/impl/UserServiceImpl.java
@@ -3,9 +3,13 @@ package com.congzhou.service.impl;
 import com.congzhou.dao.UserDao;
 import com.congzhou.service.UserService;
 import com.congzhou.util.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 public class UserServiceImpl implements UserService {
     private UserDao userDao;
+
+    @Autowired
     private Logger logger;
 
 //    Spring的DI有两种方式：基于构造函数或基于setter方法，配置文件中对应<constructor-arg>和<property>标签，两种方式可以混用
@@ -17,10 +21,6 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
-    public void setLogger(Logger logger){
-        this.logger = logger;
-    }
-
     @Override
     public void say(){
         System.out.println("hey yeah");
@@ -28,7 +28,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void getList(){
-        this.logger.log("获取用户列表");
+        logger.log("获取用户列表");
         this.userDao.getList();
     }
 }
