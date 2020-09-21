59c08b34cdbbb57e95dcbd47f4d16932674493dc
congzhou09 <yuancongzhou@yeah.net>
2020-09-21 17:05:40 +0800

    增加aop的支持与应用

--- src/main/java/com/congzhou/Application.java
+++ src/main/java/com/congzhou/Application.java
@@ -1,6 +1,7 @@
 package com.congzhou;
 
-import com.congzhou.service.impl.UserServiceImpl;
+import com.congzhou.service.RoleService;
+import com.congzhou.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,8 +9,11 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext appContext = new ClassPathXmlApplicationContext("app-bean.xml");
 
-        UserServiceImpl oneUserService = (UserServiceImpl) appContext.getBean("userService");
+        UserService oneUserService = (UserService) appContext.getBean("userService");
         oneUserService.say(); // 验证IoC： 打印 hey yeah
         oneUserService.getList(); // 验证DI： 打印 get user list ok
+
+        RoleService oneRoleService = (RoleService) appContext.getBean("roleService");
+        oneRoleService.say();
     }
 }
